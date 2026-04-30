0b4273c18d2ecebe12db3e5424d5b684a87519a0
Samantha Hyatt <samanthahyatt@MacBook-Pro.local>
2026-04-29 18:26:17 -0600

    updated dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,33 +1,22 @@
+import { useEffect, useState } from "react";
+import TripCard from "../components/TripCard";
+
+
 function Dashboard() {
-  const trips = [
-    {
-      id: 1,
-      name: "Santa Fe Girls Trip",
-      location: "Santa Fe, NM",
-      startDate: "June 12",
-      endDate: "June 14",
-    },
-    {
-      id: 2,
-      name: "Greenville Family Trip",
-      location: "Greenville, SC",
-      startDate: "July 24",
-      endDate: "July 29",
-    },
-  ];
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/trips`)
+      .then((response) => response.json())
+      .then((data) => setTrips(data));
+  }, []);
 
   return (
     <div>
       <h1>Upcoming Trips</h1>
 
       {trips.map((trip) => (
-        <div key={trip.id}>
-          <h2>{trip.name}</h2>
-          <p>{trip.location}</p>
-          <p>
-            {trip.startDate} - {trip.endDate}
-          </p>
-        </div>
+        <TripCard key={trip.id} trip={trip} />
       ))}
     </div>
   );
